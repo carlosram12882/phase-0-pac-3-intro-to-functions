3feb046fa3c847f11a057b21b4c003302923d357
Carlos Ramirez <carlosramirez686@gmail.com>
2021-12-15 16:50:53 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Follow along with the examples here
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`;
+  console.log("I was called!");
+}
+console.log(say("Howdy", "partner"));
+function say(greeting, firstName) {
+  console.log("I was called!");
+  return `${greeting}, ${firstName}!`;
+}
